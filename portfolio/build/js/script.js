f84376cd7d3f76432c985e3fd9321ad9b4bfe677
Veligurov <Veligurov1996@mail.ru>
2019-07-31 18:34:34 +0300

    Это мой первый коммит

---
+++ portfolio/build/js/script.js
@@ -0,0 +1,1 @@
+!function(n){n(function(){n(".menu__icon").on("click",function(){n(this).closest(".menu").toggleClass("menu_state_open")}),n(".menu__link").on("click",function(){n(this).closest(".menu").removeClass("menu_state_open")})})}(jQuery);
